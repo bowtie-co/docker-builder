d3d69adbff947f06f4c77105605b0e16020aaf8c
Charlie McClung <charlie@cmr1.com>
2018-04-26 22:02:49 -0600

    Add entrypoint lines from CodeBuild example
    https://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker-custom-image.html

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-dockerd >> /var/log/dockerd.log 2>&1 &
+nohup /usr/local/bin/dockerd --host=unix:///var/run/docker.sock --host=tcp://127.0.0.1:2375 --storage-driver=overlay&
+timeout -t 15 sh -c "until docker info; do echo .; sleep 1; done"
 
 exec "$@"
