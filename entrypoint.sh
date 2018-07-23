b7f6223fb3a3d3939ce1b2ff0fbb5db53dd11bec
Brandon Cabael <brandon@bowtie.co>
2018-07-23 13:36:13 -0600

    Implemented fix for deploy-ecs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export PATH="$BOWTIE_BIN:$PATH"
+# export PATH="$BOWTIE_BIN:$PATH"
 
 nohup /usr/local/bin/dockerd --host=unix:///var/run/docker.sock --host=tcp://127.0.0.1:2375 --storage-driver=overlay&
 timeout -t 15 sh -c "until docker info; do echo .; sleep 1; done"
