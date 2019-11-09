f196d3d6187950ca188fdbc04cb53e7edb0efedb
Charlie McClung <charlie@bowtie.co>
2019-11-08 22:25:32 -0700

    sleep for 2 seconds after loading docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 nohup /usr/local/bin/dockerd --host=unix:///var/run/docker.sock --host=tcp://127.0.0.1:2375 --storage-driver=overlay&
+
+sleep 2
+
 timeout -t 15 sh -c "until docker info; do echo .; sleep 1; done"
 
 exec "$@"
